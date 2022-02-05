b8107b177bd33980d7314334ac2d9e4e28a94e17
Shubh Agrawal <shubhagrawal1280@gmail.com>
2022-02-05 08:32:47 +0530

    placingmark on board

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-const box1 = document.querySelector(".inner-box");
-console.log(box1);
+const X_CLASS = "x";
+const CIRCLE_CLASS = "circle";
+let circlesTurn = false;
+const cells = document.querySelectorAll("[data-cell]");
+const board = document.getElementById("board");
 
-//event listeners
-box1.addEventListener("mouseover", hoverEvent);
+startGame();
 
-//event listener methods
-function hoverEvent() {
-  console.log("Hello");
+function startGame() {
+  circlesTurn = false;
+  cells.forEach((cell) => {
+    cell.addEventListener("click", handleClick, { once: true });
+  });
+}
+
+function handleClick(event) {
+  const cell = event.target;
+  const currentClass = circlesTurn ? CIRCLE_CLASS : X_CLASS;
+  placeMark(cell, currentClass);
+  switchTurns();
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass);
+}
+
+function switchTurns() {
+  circlesTurn = !circlesTurn;
 }
